c365a71453c49cd2805c076e9c3778a654a61349
IT诸葛亮 <dockerhome@126.com>
2018-05-04 15:32:51 +0800

    upate deplpy shells

--- doc/testFastpayMaster.sh
+++ doc/testFastpayMaster.sh
@@ -1,21 +1,33 @@
+#author cuijun luke 201804  pre-shell
+cd $WORKSPACE
+#ls
+echo "###############update weixinpay_config.properties file now##################"
+cp ~/weixinpay_config.properties fast-pay-service/src/main/resources/weixinpay_config.properties
+
+###############################################################################################
+# post-shell
 BUILD_ID=dontKillMeTomcatMaster
 #author cuijun luke 201804
 echo "start check WORKSPACE dir on jenkins server"
 cd $WORKSPACE
 ls
+echo $ref
 
 tomcatpid=`ps -ef | grep tomcat | grep -v grep | awk '{print $2}'`
 
 if test -z ${tomcatpid}
 then
-    echo $(date +%F" "%T)"--info--:tomcatpid not exsit,so pls go on 成功" 
+    echo $(date +%F" "%T)"--info--:tomcatpid not exsit,so pls go on 鎴愬姛" 
 else
-    echo $(date +%F" "%T)"--info--:tomcatpid进程存在，pid="${tomcatpid}
+    echo $(date +%F" "%T)"--info--:tomcatpid杩涚▼瀛樺湪锛宲id="${tomcatpid}
     kill -9 ${tomcatpid}
 fi
 
 netstat -ntlp 
-
+#clear tomcat cache
+cd /mnt/apache-tomcat-9.0.6/work/Catalina
+rm -rf localhost
+#clear tomcat old wars
 cd /mnt/apache-tomcat-9.0.6/webapps
 rm -rf fast-pay-web-boss
 rm -rf fast-pay-web-gateway
@@ -27,6 +39,7 @@ rm -f fast-pay-web-merchant.war
 rm -f fast-pay-web-sample-shop.war
 ls
 
+#copy newest wars
 cp $WORKSPACE/fast-pay-web-boss/target/fast-pay-web-boss.war  /mnt/apache-tomcatwars/
 cp $WORKSPACE/fast-pay-web-gateway/target/fast-pay-web-gateway.war /mnt/apache-tomcatwars/
 cp $WORKSPACE/fast-pay-web-merchant/target/fast-pay-web-merchant.war /mnt/apache-tomcatwars/
@@ -45,5 +58,8 @@ nohup ./startup.sh &
 sleep 2s
 netstat -ntlp
 ps -ef | grep tomcat
+
+sleep 5s
+#cp ~/weixinpay_config.properties $fastpay/fast-pay-service/src/main/resources/weixinpay_config.properties
 
 exit 0
